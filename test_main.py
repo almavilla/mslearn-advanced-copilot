f77397c7a5ecd5eabcd0d9e62a08fe70b81a3962
Alma La Rocco <almalarocco@gmail.com>
2026-06-09 13:13:12 -0400

    Added United States

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ def test_root():
 def test_countries():
     response = client.get("/countries")
     assert response.status_code == 200
-    assert sorted(response.json()) == ["England", "France", "Germany", "Italy", "Mexico", "Peru", "Portugal", "Spain"]
+    assert sorted(response.json()) == ["England", "France", "Germany", "Italy", "Mexico", "Peru", "Portugal", "Spain", "United States"]
 
 
 def test_cities_for_country():
